e9ffe8e8727e60e4a7f58f37c2da1f4eaedc9dc5
Imigkos <68186003+Imigkos@users.noreply.github.com>
2022-11-28 20:20:56 +0200

    Add files via upload

--- Project/main.c
+++ Project/main.c
@@ -4,20 +4,16 @@ void main()
 {
 
     int author_arr_size = get_arr_size("authors.txt");
-    author * author_arr = malloc(sizeof(author) * author_arr_size); 
+    author *author_arr = malloc(sizeof(author) * author_arr_size);
     author_arr = get_authors();
-   
 
     int book_arr_size = get_arr_size("book.txt");
-    book * book_arr = malloc(sizeof(book)*book_arr_size);
+    book *book_arr = malloc(sizeof(book) * book_arr_size);
     book_arr = get_books();
-    
 
     int writes_arr_size = get_arr_size("writes.txt");
-    writes * writes_arr = malloc(sizeof(writes)*writes_arr_size);
+    writes *writes_arr = malloc(sizeof(writes) * writes_arr_size);
     writes_arr = get_writes();
-    
-    
 
     int exit = 0;
     while (exit == 0)
@@ -26,38 +22,37 @@ void main()
         printf("\n1)Insert new writer record\n2)Insert new book record\n3)Search a writer record\n4)Search a book record\n5)Delete a writer record\n6)Delete a book record\n7)Exit\n");
         scanf("%d", &menu_choice);
         getchar();
-       
-        
 
         switch (menu_choice)
         {
         case 1:
-            
-            author_arr = insert_author(author_arr_size,author_arr);
+
+            author_arr = insert_author(author_arr_size, author_arr);
             author_arr_size++;
-        
+
             break;
         case 2:
             char surname_target[MAX];
 
-            book_arr = insert_book(book_arr_size,book_arr);
-            book_arr_size++; 
+            book_arr = insert_book(book_arr_size, book_arr);
+            book_arr_size++;
 
             printf("\nGive the surname of the book's author: ");
-            scanf("%s",&surname_target);
-            int target_author = search_author(author_arr,author_arr_size,surname_target);
-            if (target_author == -1){
+            scanf("%s", &surname_target);
+            int target_author = search_author(author_arr, author_arr_size, surname_target);
+            if (target_author == -1)
+            {
                 printf("\nAuthor was not found in the database: \n");
-                author_arr = insert_author(author_arr_size,author_arr);
+                author_arr = insert_author(author_arr_size, author_arr);
                 author_arr_size++;
-                writes_arr = insert_writes(writes_arr_size,book_arr_size,author_arr_size,surname_target,writes_arr,book_arr,author_arr);
-                writes_arr_size ++;
+                writes_arr = insert_writes(writes_arr_size, book_arr_size, author_arr_size, surname_target, writes_arr, book_arr, author_arr);
+                writes_arr_size++;
             }
-            else{
-                
-                writes_arr = insert_writes(writes_arr_size,book_arr_size,author_arr_size,surname_target,writes_arr,book_arr,author_arr);
-                writes_arr_size ++;
+            else
+            {
 
+                writes_arr = insert_writes(writes_arr_size, book_arr_size, author_arr_size, surname_target, writes_arr, book_arr, author_arr);
+                writes_arr_size++;
             }
 
             break;
@@ -65,37 +60,41 @@ void main()
             target_author = 0;
             printf("\nEnter the author's surname: ");
             scanf("%s", &surname_target);
-            target_author=search_author(author_arr,author_arr_size,surname_target);
-            if (target_author == -1){
+            target_author = search_author(author_arr, author_arr_size, surname_target);
+            if (target_author == -1)
+            {
                 printf("\nAuthor was not found in the database\n");
             }
-            else{
-                printf("\nThe author's full name is: %s %s",author_arr[target_author].name,author_arr[target_author].surname);
-                search_writes_by_id(writes_arr,book_arr,writes_arr_size,book_arr_size,author_arr[target_author].writer_id);
+            else
+            {
+                printf("\nThe author's full name is: %s %s", author_arr[target_author].name, author_arr[target_author].surname);
+                search_writes_by_id(writes_arr, book_arr, writes_arr_size, book_arr_size, author_arr[target_author].writer_id);
             }
             break;
         case 4:
             char title_target[MAX];
             int target_book;
             printf("Enter the book's title: ");
-            fgets(title_target,MAX,stdin);
+            fgets(title_target, MAX, stdin);
             title_target[strcspn(title_target, "\n")] = 0;
-            target_book = search_book(book_arr,book_arr_size,title_target);
-            if (target_book == -1){
+            target_book = search_book(book_arr, book_arr_size, title_target);
+            if (target_book == -1)
+            {
                 printf("\nBook was not found in the database");
             }
-            else{
-                int target_writes = search_writes_by_title(writes_arr,writes_arr_size,title_target);
-                int current_book_author = search_author_id(author_arr,author_arr_size,target_writes);
+            else
+            {
+                int target_writes = search_writes_by_title(writes_arr, writes_arr_size, title_target);
+                int current_book_author = search_author_id(author_arr, author_arr_size, target_writes);
                 printf("\n-----------------");
-                printf("\n%s",book_arr[target_book].title);
-                printf("\n%d",book_arr[target_book].release_date);
-                printf("\n%.2f€",book_arr[target_book].price);
+                printf("\n%s", book_arr[target_book].title);
+                printf("\n%d", book_arr[target_book].release_date);
+                printf("\n%.2f€", book_arr[target_book].price);
                 printf("\n-----------------");
                 printf("\nWritten By: ");
                 printf("\n-----------------");
-                printf("\n%s",author_arr[current_book_author].name);
-                printf("\n%s",author_arr[current_book_author].surname);
+                printf("\n%s", author_arr[current_book_author].name);
+                printf("\n%s", author_arr[current_book_author].surname);
                 printf("\n-----------------\n");
             }
             break;
@@ -103,16 +102,42 @@ void main()
             printf("delete writer");
             break;
         case 6:
-            printf("delete book");
+
+            char *book_title = malloc(512 * sizeof(char));
+            printf("\nEnter the title of the book you want to delete: ");
+            fgets(book_title, MAX, stdin);
+            book_title[strcspn(book_title, "\n")] = 0;
+            int book_index = search_book(book_arr, book_arr_size, book_title);
+
+            if (book_index != -1)
+            {
+                book_arr = delete_book(book_arr, book_title, book_arr_size, book_index);
+                book_arr_size--;
+            }
+            else
+            {
+                printf("\nBook was not found :P");
+            }
+
             break;
+
         case 7:
-              for (int i = 0; i < author_arr_size; i++)
-    {
-           printf("\n%s\n%s\n%d\n%d\n",author_arr[i].name,author_arr[i].surname,author_arr[i].writer_id,author_arr[i].num_of_books);
-    }
             exit = 1;
-            qsort(author_arr,author_arr_size,sizeof(author),author_id_compare);
-            exit_library(author_arr,book_arr,writes_arr,author_arr_size,book_arr_size,writes_arr_size);
+            for (int i = 0;i<book_arr_size;i++){
+                printf("-----------\n%s",book_arr[i].title);
+                printf("\n%.2f",book_arr[i].price);
+                printf("\n%d",book_arr[i].release_date);
+                printf("\n-----------");
+            }
+            // for (int i = 0;i<author_arr_size;i++){
+            //     printf("-----------\n%s",author_arr[i].name);
+            //     printf("\n%d",author_arr[i].writer_id);
+            //     printf("\n%d",author_arr[i].num_of_books);
+            //     printf("\n-----------");
+            // }
+
+            qsort(author_arr, author_arr_size, sizeof(author), author_id_compare);
+            exit_library(author_arr, book_arr, writes_arr, author_arr_size, book_arr_size, writes_arr_size);
             free(author_arr);
             free(book_arr);
             free(writes_arr);
--- Project/main_functions.c
+++ Project/main_functions.c
@@ -1,11 +1,12 @@
 #include "init.h"
 #define MAX 35
 
-author * get_authors(){
+author *get_authors()
+{
 
     FILE *author_file;
 
-    if((author_file= fopen("authors.txt", "r"))==NULL)
+    if ((author_file = fopen("authors.txt", "r")) == NULL)
     {
         printf("File does not exist or cannot be opened.\n");
         exit(1);
@@ -14,37 +15,38 @@ author * get_authors(){
     int i = 0;
     char buffer[35];
 
-    fgets (buffer, MAX ,author_file); 
+    fgets(buffer, MAX, author_file);
     entries = atoi(buffer);
-    author * author_arr = malloc(sizeof(author) * entries); 
-    while (fgets(buffer,MAX,author_file)){
+    author *author_arr = malloc(sizeof(author) * entries);
+    while (fgets(buffer, MAX, author_file))
+    {
         author writer;
         writer.writer_id = atoi(buffer);
-        //char* buffer = malloc(100*sizeof(char));
+        // char* buffer = malloc(100*sizeof(char));
 
-        writer.surname = malloc(sizeof(char)*MAX);
-        fgets(writer.surname,MAX,author_file);
+        writer.surname = malloc(sizeof(char) * MAX);
+        fgets(writer.surname, MAX, author_file);
         writer.surname[strcspn(writer.surname, "\n")] = 0;
-        
-        writer.name = malloc (sizeof(char)*MAX);
-        fgets(writer.name,MAX,author_file);
+
+        writer.name = malloc(sizeof(char) * MAX);
+        fgets(writer.name, MAX, author_file);
         writer.name[strcspn(writer.name, "\n")] = 0;
 
-        fgets(buffer,MAX,author_file);
+        fgets(buffer, MAX, author_file);
         writer.num_of_books = atoi(buffer);
 
         author_arr[i] = writer;
         i++;
-
     }
     return author_arr;
 }
 
-book * get_books(){
+book *get_books()
+{
 
     FILE *book_file;
 
-    if((book_file= fopen("book.txt", "r"))==NULL)
+    if ((book_file = fopen("book.txt", "r")) == NULL)
     {
         printf("File does not exist or cannot be opened.\n");
         exit(1);
@@ -53,33 +55,34 @@ book * get_books(){
     int i = 0;
     char buffer[35];
 
-    fgets (buffer, MAX ,book_file); 
+    fgets(buffer, MAX, book_file);
     entries = atoi(buffer);
-    book * book_arr = malloc(sizeof(book) * entries); 
-    while (fgets(buffer,MAX,book_file)){
+    book *book_arr = malloc(sizeof(book) * entries);
+    while (fgets(buffer, MAX, book_file))
+    {
         book book;
         book.release_date = atoi(buffer);
-        //char* buffer = malloc(100*sizeof(char));
+        // char* buffer = malloc(100*sizeof(char));
 
-        book.title= malloc(sizeof(char)*MAX);
-        fgets(book.title,MAX,book_file);
+        book.title = malloc(sizeof(char) * MAX);
+        fgets(book.title, MAX, book_file);
         book.title[strcspn(book.title, "\n")] = 0;
 
-        fgets(buffer,MAX,book_file);
+        fgets(buffer, MAX, book_file);
         book.price = atof(buffer);
 
         book_arr[i] = book;
         i++;
-
     }
     return book_arr;
 }
 
-writes * get_writes(){
+writes *get_writes()
+{
 
     FILE *writes_file;
 
-    if((writes_file= fopen("writes.txt", "r"))==NULL)
+    if ((writes_file = fopen("writes.txt", "r")) == NULL)
     {
         printf("File does not exist or cannot be opened.\n");
         exit(1);
@@ -88,27 +91,27 @@ writes * get_writes(){
     int i = 0;
     char buffer[35];
 
-    fgets (buffer, MAX ,writes_file); 
+    fgets(buffer, MAX, writes_file);
     entries = atoi(buffer);
-    writes * writes_arr = malloc(sizeof(writes) * entries); 
-    while (fgets(buffer,MAX,writes_file)){
+    writes *writes_arr = malloc(sizeof(writes) * entries);
+    while (fgets(buffer, MAX, writes_file))
+    {
         writes writes;
         writes.writer_id = atoi(buffer);
-        //char* buffer = malloc(100*sizeof(char));
+        // char* buffer = malloc(100*sizeof(char));
 
-        writes.title= malloc(sizeof(char)*MAX);
-        fgets(writes.title,MAX,writes_file);
+        writes.title = malloc(sizeof(char) * MAX);
+        fgets(writes.title, MAX, writes_file);
         writes.title[strcspn(writes.title, "\n")] = 0;
 
         writes_arr[i] = writes;
         i++;
-
     }
     return writes_arr;
 }
 
-
-int get_arr_size(char *filename){
+int get_arr_size(char *filename)
+{
     FILE *file;
     if ((file = fopen(filename, "r")) == NULL)
     {
@@ -118,97 +121,96 @@ int get_arr_size(char *filename){
     }
     char str[35];
     int array_size;
-    fgets(str,MAX,file);
+    fgets(str, MAX, file);
     array_size = atoi(str);
-    return(array_size);
+    return (array_size);
 }
 
-author * insert_author(int arr_size,author *author_arr)
+author *insert_author(int arr_size, author *author_arr)
 {
     author new_writer;
 
-    char *buffer = malloc(512*sizeof(char));
+    char *buffer = malloc(512 * sizeof(char));
     printf("\nEnter the author's name: ");
-    scanf("%s",buffer);
-    new_writer.name = malloc(strlen(buffer)*sizeof(char));
-    strcpy(new_writer.name,buffer);
+    scanf("%s", buffer);
+    new_writer.name = malloc(strlen(buffer) * sizeof(char));
+    strcpy(new_writer.name, buffer);
     free(buffer);
     buffer = NULL;
 
-    buffer = malloc(512*sizeof(char));
+    buffer = malloc(512 * sizeof(char));
     printf("\nEnter the author's surname: ");
-    scanf("%s",buffer);
-    new_writer.surname = malloc(strlen(buffer)*sizeof(char));
-    strcpy(new_writer.surname,buffer);
+    scanf("%s", buffer);
+    new_writer.surname = malloc(strlen(buffer) * sizeof(char));
+    strcpy(new_writer.surname, buffer);
     free(buffer);
     buffer = NULL;
 
-    new_writer.writer_id = (arr_size+1);
+    new_writer.writer_id = (arr_size + 1);
     new_writer.num_of_books = 0;
-    author_arr = realloc(author_arr,sizeof(author_arr)*sizeof(author)+sizeof(author_arr)*sizeof(author));
+    author_arr = realloc(author_arr, sizeof(author_arr) * sizeof(author) + sizeof(author_arr) * sizeof(author));
     author_arr[arr_size] = new_writer;
-    return(author_arr);
+    return (author_arr);
 }
 
-void exit_library(author *author_arr,book *book_arr,writes *writes_arr,int author_arr_size,int book_arr_size,int writes_arr_size){
+void exit_library(author *author_arr, book *book_arr, writes *writes_arr, int author_arr_size, int book_arr_size, int writes_arr_size)
+{
 
-    FILE *author_file,*writes_file,*book_file;
+    FILE *author_file, *writes_file, *book_file;
     int i;
 
-    if ((author_file= fopen("authors.txt", "w")) == NULL)
+    if ((author_file = fopen("authors.txt", "w")) == NULL)
     {
         fprintf(stderr, "Error in authors.txt");
         exit(1);
     }
 
-for (i = 0; i < author_arr_size; i++)
+    for (i = 0; i < author_arr_size; i++)
     {
-        if (i==0)
+        if (i == 0)
         {
-           fprintf(author_file,"%d\n",author_arr_size);
+            fprintf(author_file, "%d\n", author_arr_size);
         }
-        
+
         fprintf(author_file, "%d\n", author_arr[i].writer_id);
         fprintf(author_file, "%s\n", author_arr[i].surname);
         fprintf(author_file, "%s\n", author_arr[i].name);
         fprintf(author_file, "%d\n", author_arr[i].num_of_books);
     }
 
-if ((book_file= fopen("book.txt", "w")) == NULL)
+    if ((book_file = fopen("book.txt", "w")) == NULL)
     {
         fprintf(stderr, "Error in book.txt");
         exit(1);
     }
 
-for (i = 0; i < book_arr_size; i++)
+    for (i = 0; i < book_arr_size; i++)
     {
-        if (i==0)
+        if (i == 0)
         {
-           fprintf(book_file,"%d\n",book_arr_size);
+            fprintf(book_file, "%d\n", book_arr_size);
         }
-        
+
         fprintf(book_file, "%d\n", book_arr[i].release_date);
         fprintf(book_file, "%s\n", book_arr[i].title);
         fprintf(book_file, "%.2f\n", book_arr[i].price);
-        
     }
 
-if ((writes_file= fopen("writes.txt", "w")) == NULL)
+    if ((writes_file = fopen("writes.txt", "w")) == NULL)
     {
         fprintf(stderr, "Error in writes.txt");
         exit(1);
     }
 
-for (i = 0; i < writes_arr_size; i++)
+    for (i = 0; i < writes_arr_size; i++)
     {
-        if (i==0)
+        if (i == 0)
         {
-           fprintf(writes_file,"%d\n",writes_arr_size);
+            fprintf(writes_file, "%d\n", writes_arr_size);
         }
-        
+
         fprintf(writes_file, "%d\n", writes_arr[i].writer_id);
         fprintf(writes_file, "%s\n", writes_arr[i].title);
-           
     }
 
     fflush(author_file);
@@ -219,191 +221,255 @@ for (i = 0; i < writes_arr_size; i++)
 
     fflush(writes_file);
     fclose(writes_file);
-
 }
 
-int author_surname_compare(author *writer1,author *writer2){
-    int surname_compare = strcasecmp(writer1->surname,writer2->surname);
+int author_surname_compare(author *writer1, author *writer2)
+{
+    int surname_compare = strcasecmp(writer1->surname, writer2->surname);
     return surname_compare;
 }
 
-int author_id_compare(author *writer1,author *writer2){
-    if (writer1->writer_id>writer2->writer_id){
+int author_id_compare(author *writer1, author *writer2)
+{
+    if (writer1->writer_id > writer2->writer_id)
+    {
         return 1;
     }
-    else if (writer1->writer_id<writer2->writer_id)
+    else if (writer1->writer_id < writer2->writer_id)
     {
         return -1;
     }
-    else{
-        return 0;
-    }
-    
-}
-
-int book_title_compare(book *book1,book *book2){
-    int title_compare = strcasecmp(book1->title,book2->title);
-    return title_compare;
-}
-
-int writes_title_compare(writes *writes1,writes *writes2){
-    int title_compare = strcasecmp(writes1->title,writes2->title);
-    return title_compare;
-}
-int writes_id_compare(writes *writes1,writes *writes2){
-    if (writes1->writer_id>writes2->writer_id){
-        return 1;
-    }
-    else if (writes1->writer_id<writes2->writer_id)
+    else
     {
-        return -1;
-    }
-    else{
         return 0;
     }
 }
 
-int search_author(author *author_arr, int array_size, char *surname){
-    int bottom= 0;
+int book_title_compare(book *book1, book *book2)
+{
+    int title_compare = strcasecmp(book1->title, book2->title);
+    return title_compare;
+}
+
+int writes_title_compare(writes *writes1, writes *writes2)
+{
+    int title_compare = strcasecmp(writes1->title, writes2->title);
+    return title_compare;
+}
+int writes_id_compare(writes *writes1, writes *writes2)
+{
+    if (writes1->writer_id > writes2->writer_id)
+    {
+        return 1;
+    }
+    else if (writes1->writer_id < writes2->writer_id)
+    {
+        return -1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+int search_author(author *author_arr, int array_size, char *surname)
+{
+    int bottom = 0;
     int mid;
     int top = array_size - 1;
-    
-     
-    qsort(author_arr,array_size,sizeof(author),author_surname_compare);
 
+    qsort(author_arr, array_size, sizeof(author), author_surname_compare);
 
-    while(bottom <= top){
-        mid = (bottom + top)/2;
-        if (strcasecmp(author_arr[mid].surname, surname) == 0){
-            
-            return(mid);
-        } else if (strcasecmp(author_arr[mid].surname, surname) > 0){
+    while (bottom <= top)
+    {
+        mid = (bottom + top) / 2;
+        if (strcasecmp(author_arr[mid].surname, surname) == 0)
+        {
+
+            return (mid);
+        }
+        else if (strcasecmp(author_arr[mid].surname, surname) > 0)
+        {
             top = mid - 1;
-        } else if (strcasecmp(author_arr[mid].surname, surname) < 0){
+        }
+        else if (strcasecmp(author_arr[mid].surname, surname) < 0)
+        {
             bottom = mid + 1;
         }
     }
-    
+
     return -1;
 }
 
-int search_book(book *book_arr,int array_size,char *title){
-    int bottom= 0;
-    int mid;
-    int top = array_size - 1;
-    char *buffer;
-
-    qsort(book_arr,array_size,sizeof(book),book_title_compare);
-
-    while(bottom <= top){
-        mid = (bottom + top)/2;
-        if (strcasecmp(book_arr[mid].title ,title) == 0){
-            return(mid);
-        } else if (strcasecmp(book_arr[mid].title, title) > 0){
-            top = mid - 1;
-        } else if (strcasecmp(book_arr[mid].title, title) < 0){
-            bottom = mid + 1;
-        }
-    }
-    return -1;
-}
-
-int search_writes_by_title(writes *writes_arr,int array_size,char *title){
-    int bottom= 0;
-    int mid;
-    int top = array_size - 1;
-    char *buffer;
-
-
-    qsort(writes_arr,array_size,sizeof(writes),writes_title_compare);
-
-    while(bottom <= top){
-        mid = (bottom + top)/2;
-        if (strcasecmp(writes_arr[mid].title ,title) == 0){
-            return(writes_arr[mid].writer_id);
-        } else if (strcasecmp(writes_arr[mid].title, title) > 0){
-            top = mid - 1;
-        } else if (strcasecmp(writes_arr[mid].title, title) < 0){
-            bottom = mid + 1;
-        }
-    }
-    return -1;
-}
-
-int search_author_id(author *author_arr, int array_size, int id){
-    int bottom= 0;
+int search_book(book *book_arr, int array_size, char *title)
+{
+    int bottom = 0;
     int mid;
     int top = array_size - 1;
     char *buffer;
 
-    qsort(author_arr,array_size,sizeof(author),author_id_compare);
+    qsort(book_arr, array_size, sizeof(book), book_title_compare);
 
-    while(bottom <= top){
-        mid = (bottom + top)/2;
-        if (author_arr[mid].writer_id==id){
-            return(mid);
-        } else if (author_arr[mid].writer_id>id) {
+    while (bottom <= top)
+    {
+        mid = (bottom + top) / 2;
+        if (strcasecmp(book_arr[mid].title, title) == 0)
+        {
+            return (mid);
+        }
+        else if (strcasecmp(book_arr[mid].title, title) > 0)
+        {
             top = mid - 1;
-        } else if (author_arr[mid].writer_id<id){
+        }
+        else if (strcasecmp(book_arr[mid].title, title) < 0)
+        {
             bottom = mid + 1;
         }
     }
-    
     return -1;
 }
 
-void search_writes_by_id(writes *writes_arr,book *book_arr,int writes_array_size,int book_arr_size,int id){
+int search_writes_by_title(writes *writes_arr, int array_size, char *title)
+{
+    int bottom = 0;
+    int mid;
+    int top = array_size - 1;
+    char *buffer;
+
+    qsort(writes_arr, array_size, sizeof(writes), writes_title_compare);
+
+    while (bottom <= top)
+    {
+        mid = (bottom + top) / 2;
+        if (strcasecmp(writes_arr[mid].title, title) == 0)
+        {
+            return (writes_arr[mid].writer_id);
+        }
+        else if (strcasecmp(writes_arr[mid].title, title) > 0)
+        {
+            top = mid - 1;
+        }
+        else if (strcasecmp(writes_arr[mid].title, title) < 0)
+        {
+            bottom = mid + 1;
+        }
+    }
+    return -1;
+}
+
+int search_author_id(author *author_arr, int array_size, int id)
+{
+    int bottom = 0;
+    int mid;
+    int top = array_size - 1;
+    char *buffer;
+
+    qsort(author_arr, array_size, sizeof(author), author_id_compare);
+
+    while (bottom <= top)
+    {
+        mid = (bottom + top) / 2;
+        if (author_arr[mid].writer_id == id)
+        {
+            return (mid);
+        }
+        else if (author_arr[mid].writer_id > id)
+        {
+            top = mid - 1;
+        }
+        else if (author_arr[mid].writer_id < id)
+        {
+            bottom = mid + 1;
+        }
+    }
+
+    return -1;
+}
+
+void search_writes_by_id(writes *writes_arr, book *book_arr, int writes_array_size, int book_arr_size, int id)
+{
     int has_books = 0;
-    qsort(writes_arr,writes_array_size,sizeof(writes),writes_id_compare);
-    for(int i=0;i<writes_array_size;i++){
-        if (writes_arr[i].writer_id == id){
-            int found_book = search_book(book_arr,book_arr_size,writes_arr[i].title);
+    qsort(writes_arr, writes_array_size, sizeof(writes), writes_id_compare);
+    for (int i = 0; i < writes_array_size; i++)
+    {
+        if (writes_arr[i].writer_id == id)
+        {
+            int found_book = search_book(book_arr, book_arr_size, writes_arr[i].title);
             printf("\n-----------------");
-            printf("\n%s",book_arr[found_book].title);
-            printf("\n%d",book_arr[found_book].release_date);
-            printf("\n%.2f€",book_arr[found_book].price);
+            printf("\n%s", book_arr[found_book].title);
+            printf("\n%d", book_arr[found_book].release_date);
+            printf("\n%.2f€", book_arr[found_book].price);
             printf("\n-----------------");
             has_books = 1;
         }
     }
-    if (has_books == 0){
+    if (has_books == 0)
+    {
         printf("\nThere are no book entries regarding this author\n ");
     }
 }
 
-book * insert_book(int arr_size,book *book_arr)
+book *insert_book(int arr_size, book *book_arr)
 {
     book new_book;
 
-    char* buffer = malloc(512*sizeof(char));
+    char *buffer = malloc(512 * sizeof(char));
     printf("\nEnter the book's title: ");
-    fgets(buffer,MAX,stdin);
+    fgets(buffer, MAX, stdin);
     buffer[strcspn(buffer, "\n")] = 0;
-    new_book.title= malloc(strlen(buffer)*sizeof(char));
-    strcpy(new_book.title,buffer);
+    new_book.title = malloc(strlen(buffer) * sizeof(char));
+    strcpy(new_book.title, buffer);
     free(buffer);
     buffer = NULL;
 
     printf("\nEnter the book's release date: ");
-    scanf("%d",&new_book.release_date);
+    scanf("%d", &new_book.release_date);
 
     printf("\nEnter the book's price: ");
-    scanf("%f",&new_book.price);
-    book_arr = realloc(book_arr,sizeof(book_arr)*sizeof(book)+sizeof(book_arr)*sizeof(book));
+    scanf("%f", &new_book.price);
+    book_arr = realloc(book_arr, sizeof(book_arr) * sizeof(book) + sizeof(book_arr) * sizeof(book));
     book_arr[arr_size] = new_book;
-    return(book_arr);
+    return (book_arr);
 }
 
-writes *insert_writes(int writes_arr_size,int book_arr_size,int author_arr_size,char *surname,writes *writes_arr, book *book_arr,author* author_arr){
-    
+writes *insert_writes(int writes_arr_size, int book_arr_size, int author_arr_size, char *surname, writes *writes_arr, book *book_arr, author *author_arr)
+{
+
     writes new_writes;
 
-    new_writes.title = malloc(strlen(book_arr[book_arr_size-1].title)*sizeof(char));
-    strcpy(new_writes.title,book_arr[book_arr_size-1].title);
+    new_writes.title = malloc(strlen(book_arr[book_arr_size - 1].title) * sizeof(char));
+    strcpy(new_writes.title, book_arr[book_arr_size - 1].title);
 
-    int index = search_author(author_arr,author_arr_size,surname);
+    int index = search_author(author_arr, author_arr_size, surname);
     new_writes.writer_id = author_arr[index].writer_id;
-    writes_arr = realloc(writes_arr,sizeof(writes_arr)*sizeof(writes)+sizeof(writes_arr)*sizeof(writes));
+    writes_arr = realloc(writes_arr, sizeof(writes_arr) * sizeof(writes) + sizeof(writes_arr) * sizeof(writes));
     writes_arr[writes_arr_size] = new_writes;
-    return(writes_arr);
+    return (writes_arr);
 }
+
+book *delete_book(book *book_arr, char *title, int book_arr_size, int book_index)
+{
+
+    if (book_index != -1)
+    {
+        book *new_book_arr;
+        new_book_arr = malloc(sizeof(book_arr_size - 1) * sizeof(book));
+        int current_pos = 0;
+        for (int i = 0; i < book_arr_size; i++)
+        {
+            if (i != book_index)
+            {
+                new_book_arr[current_pos] = book_arr[i];
+                current_pos++;
+            }
+        }
+        return(new_book_arr);
+    }
+    else
+    {
+        printf("\nBook was not found: ");
+        return (book_arr);
+    }
+}
+
+// writes *delete_writes_title
